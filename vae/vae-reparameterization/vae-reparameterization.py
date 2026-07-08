a9a202025feecfa03b3877851997c5878086124b
Minakshi654 <68809586+Minakshi654@users.noreply.github.com>
2026-07-08 21:22:33 +0530

    Solve vae-reparameterization

---
+++ vae/vae-reparameterization/vae-reparameterization.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def reparameterize(mu: np.ndarray, log_var: np.ndarray, epsilon: np.ndarray) -> np.ndarray:
+    std = np.exp(0.5 * log_var)
+    z = mu + std * epsilon
+    return z
+    
